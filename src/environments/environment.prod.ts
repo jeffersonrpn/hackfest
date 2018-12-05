e6ea7fed69a35d94088dd7cefb61ff4f8da17d47
Jefferson Neves <jefferson.rpn@gmail.com>
2018-12-05 19:57:58 -0300

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCL6WAwUbNVSglkbkR9PhZFkyawS4sEuQU",
+    authDomain: "hackfestcc.firebaseapp.com",
+    databaseURL: "https://hackfestcc.firebaseio.com",
+    projectId: "hackfestcc",
+    storageBucket: "hackfestcc.appspot.com",
+    messagingSenderId: "400199110326"
+  }
 };
